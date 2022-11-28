3cf0515fa62ca094f58bfaa3a053515dc8ea57e2
Oleg <popovolegvrn@gmail.com>
2022-11-28 16:32:00 +0300

    Added task_1 into lesson_1 dir

---
+++ lesson_1/task_1.py
@@ -0,0 +1,28 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+# Пример:
+
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+def take_args():
+    arg = int(input("Введите число обозначающую день недели: "))
+    if 0 < arg < 8:
+        return arg
+    else:
+        return take_args()
+
+
+def is_day_off(day_num):
+    if day_num <= 5:
+        return "no"
+    else:
+        return "yes"
+
+
+if __name__ == '__main__':
+    day = take_args()
+    print(is_day_off(day))
+
+
