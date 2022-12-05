883cae3a853de22b747dbcff997d35c40974533e
Oleg <popovolegvrn@gmail.com>
2022-12-05 19:14:59 +0300

    Added task_4 into lesson_3 dir

---
+++ lesson_3/task_4.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая будет
+#  преобразовывать десятичное число в двоичное.
+
+def take_args():
+    arg = int(input("Введите число: "))
+    return arg
+
+
+def formated_binary(num):
+    bin_num = bin(num).split('b')[1]
+    return bin_num
+
+
+if __name__ == '__main__':
+    try:
+        number = take_args()
+        binary_num = formated_binary(number)
+        print(binary_num)
+        my_bin_sum = my_binary_calc(number)
+        print(my_bin_sum)
+    except ValueError:
+        print('Вы ввели число в неверном формате')
