f9a5f000acc05ae5381007b23112cfb5dbb9de9d
Oleg <popovolegvrn@gmail.com>
2022-12-19 17:34:13 +0300

    Added task_3 into lesson_4 dir

---
+++ lesson_4/task_3.py
@@ -0,0 +1,18 @@
+# Задайте последовательность чисел. 
+# Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+from random import randint
+
+
+def generate_list():
+    res = []
+    for i in range (12):
+        res.append(randint(1,5))
+    return res
+
+
+if __name__ == '__main__':
+    my_list = generate_list()
+    print(f'Исходный список: \n{my_list}')
+    my_list = list(set(my_list))
+    print(f'Список неповторяющихся элементов:\n{my_list}')
